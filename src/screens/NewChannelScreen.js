f2717e832de2589a0565e842a5539b25704deef4
Rokas <rookas.rudzenskas@gmail.com>
2022-10-08 16:29:10 +0300

    Rokas

--- src/screens/NewChannelScreen.js
+++ src/screens/NewChannelScreen.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, StyleSheet, TextInput} from 'react-native';
+import Button from "../components/Button";
+import {useChatContext} from "stream-chat-expo";
 
 const NewChannelScreen = () => {
+    const [name, setName] = useState('');
+
+    const createChannel = () => {
+        // create a channel
+        const client = useChatContext();
+    }
+
     return (
-        <View>
-            <Text>
-                byrookas 🚀
-            </Text>
+        <View className="space-y-3 mx-3">
+           <TextInput autoCapitalize={'none'} autoFocus={true} value={name} onChangeText={setName} placeholder={'Channel name'} className="bg-gray-100 mt-10 mb-5 py-3 rounded-lg px-3"/>
+            <Button title={'Create channel'} onPress={createChannel} />
         </View>
     );
 };
