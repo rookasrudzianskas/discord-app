76bf2a9b527d1f20b6b23ce36988416585ac1754
Rokas <rookas.rudzenskas@gmail.com>
2022-10-08 17:31:26 +0300

    Rokas

--- src/screens/ChannelMembers.js
+++ src/screens/ChannelMembers.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {Text, View, StyleSheet, FlatList} from 'react-native';
-import {useRoute} from "@react-navigation/native";
+import {useNavigation, useRoute} from "@react-navigation/native";
 import UserListItem from "../components/UserListItem";
 import Button from "../components/Button";
 
@@ -8,6 +8,7 @@ const ChannelMembers = () => {
     const route = useRoute();
     const channel = route.params.channel;
     const [members, setMembers] = useState([]);
+    const navigation = useNavigation();
 
     const fetchMembers = async () => {
         const response = await channel.queryMembers({});
@@ -22,7 +23,11 @@ const ChannelMembers = () => {
     return (
         <View className="mt-10 mx-3">
             <FlatList
-                ListHeaderComponent={() => (<Button title={'Invite Members'} onPress={() => {}} />)}
+                ListHeaderComponent={() => (<Button title={'Invite Members'} onPress={() => {
+                    navigation.navigate('InviteMembers', {
+                        channel
+                    })
+                }} />)}
                 data={members} keyExtractor={(item) => item.user.id} showsVerticalScrollIndicator={false} renderItem={({item}) => (
                 <UserListItem user={item.user} onPress={() => {}} />
             )} />
