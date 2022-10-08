7749507202dfe2ada6ec16f9c271e4259a5702b1
Rokas <rookas.rudzenskas@gmail.com>
2022-10-08 16:45:12 +0300

    ROkas

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,5 @@
 const exclusionList = require('./metro-config/src/defaults/exclusionList');
-
+// you have to have this in case of JESTE config naming collision with aws amplify shit
 module.exports = {
     resolver: {
         blacklistRE: exclusionList([/#current-cloud-backend\/.*/]),
