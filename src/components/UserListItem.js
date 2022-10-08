ba2f0c166a07f929dbb1f15e22366efbadb43943
Rokas <rookas.rudzenskas@gmail.com>
2022-10-08 15:51:39 +0300

    Rokas

--- src/components/UserListItem.js
+++ src/components/UserListItem.js
@@ -3,7 +3,7 @@ import {Text, View, StyleSheet, Image, TouchableOpacity} from 'react-native';
 
 const UserListItem = ({user, onPress}) => {
     return (
-        <TouchableOpacity activeOpacity={0.7} className="flex-row items-center space-x-3 my-3">
+        <TouchableOpacity onPress={() => onPress(user)} activeOpacity={0.7} className="flex-row items-center space-x-3 my-3">
             <Image source={{uri: user.image}} className="w-12 h-12 rounded-full" />
             <Text className="text-white font-bold">
                 {user.name}
--- src/screens/UserListScreen.js
+++ src/screens/UserListScreen.js
@@ -2,20 +2,22 @@ import React, {useEffect, useState} from 'react';
 import {Text, View, StyleSheet, FlatList} from 'react-native';
 import {useChatContext} from "stream-chat-expo";
 import UserListItem from "../components/UserListItem";
+import {useAuthContext} from "../contexts/authContext";
 
 const UserListScreen = () => {
     const {client} = useChatContext();
     const [users, setUsers] = useState([]);
+    const {userId} = useAuthContext();
 
     const fetchUsers = async () => {
         const response = await client.queryUsers({});
         setUsers(response.users);
     }
 
-    const startChannel = async () => {
+    const startChannel = async (user) => {
         // console.log('start channel');
         const channel = client.channel('messaging', {
-            members: ['vishal', 'vishal2'],
+            members: [userId, user.id],
         });
         await channel.create();
     }
