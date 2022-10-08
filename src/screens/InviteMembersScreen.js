5b1ee07e44f93b89ae5519d26f0eebd8d92a49c9
Rokas <rookas.rudzenskas@gmail.com>
2022-10-08 17:40:49 +0300

    Rokas

--- src/screens/InviteMembersScreen.js
+++ src/screens/InviteMembersScreen.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Text, View, StyleSheet, FlatList} from 'react-native';
+import {useChatContext} from "stream-chat-expo";
+import {useAuthContext} from "../contexts/authContext";
+import {useNavigation} from "@react-navigation/native";
+import UserListItem from "../components/UserListItem";
 
 const InviteMembersScreen = () => {
+    const {client} = useChatContext();
+    const [users, setUsers] = useState([]);
+    const {userId} = useAuthContext();
+    const navigation = useNavigation();
+
+    const fetchUsers = async () => {
+        const response = await client.queryUsers({});
+        setUsers(response.users);
+    }
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
     return (
-        <View>
-            <Text className="text-white">
-                byrookas 🚀
-            </Text>
+        <View className="mt-10 mx-3">
+            <FlatList data={users} showsVerticalScrollIndicator={false} renderItem={({item}) => (
+                <UserListItem user={item} onPress={() => {}} />
+            )} />
         </View>
     );
 };
