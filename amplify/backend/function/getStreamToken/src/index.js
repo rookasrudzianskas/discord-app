cdf84fd9c0834760e49f6f9ee5c5887fc3e6a5f0
Rokas <rookas.rudzenskas@gmail.com>
2022-10-08 11:06:32 +0300

    Rokas

--- amplify/backend/function/getStreamToken/src/index.js
+++ amplify/backend/function/getStreamToken/src/index.js
@@ -5,9 +5,14 @@
 	STREAM_SECRET
 Amplify Params - DO NOT EDIT */
 
+const StreamChat = require("stream-chat").StreamChat;
+
+const { STREAM_KEY, STREAM_SECRET } = process.env;
+
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+
 exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
     return "New Generated Token";
