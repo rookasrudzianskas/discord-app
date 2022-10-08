403e39ddc37e609a98b839445d8d86def16e032e
Rokas <rookas.rudzenskas@gmail.com>
2022-10-08 15:52:36 +0300

    Rokas

--- src/screens/UserListScreen.js
+++ src/screens/UserListScreen.js
@@ -3,11 +3,13 @@ import {Text, View, StyleSheet, FlatList} from 'react-native';
 import {useChatContext} from "stream-chat-expo";
 import UserListItem from "../components/UserListItem";
 import {useAuthContext} from "../contexts/authContext";
+import {useNavigation} from "@react-navigation/native";
 
 const UserListScreen = () => {
     const {client} = useChatContext();
     const [users, setUsers] = useState([]);
     const {userId} = useAuthContext();
+    const navigation = useNavigation();
 
     const fetchUsers = async () => {
         const response = await client.queryUsers({});
@@ -20,6 +22,8 @@ const UserListScreen = () => {
             members: [userId, user.id],
         });
         await channel.create();
+        // navigate to the chat directly
+        navigation.navigate('ChannelScreen', {channel});
     }
 
     useEffect(() => {
