39e674bebb3527a976b5a841af41b0cf4ef59b40
Rokas <rookas.rudzenskas@gmail.com>
2022-10-08 15:56:07 +0300

    Rokas

--- src/screens/ChannelScreen.js
+++ src/screens/ChannelScreen.js
@@ -23,7 +23,7 @@ const ChannelScreen = () => {
     }
 
     return (
-        <Channel channel={channel} key={channel.data.name}>
+        <Channel channel={channel} key={channel.data.id}>
             <MessageList />
             <MessageInput />
         </Channel>
