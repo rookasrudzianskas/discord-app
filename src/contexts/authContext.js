f3a8c1f762102261a79a51ed3caf90373e2ee4a1
Rokas <rookas.rudzenskas@gmail.com>
2022-10-08 15:12:31 +0300

    Rokas

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -1,5 +1,6 @@
 import { createContext, useState, useContext, useEffect } from "react";
 import {Auth} from "aws-amplify";
+import {getStreamToken} from '../graphql/queries';
 
 const AuthContext = createContext({
     userId: null,
@@ -24,6 +25,7 @@ const AuthContextComponent = ({ children, client }) => {
                     "https://yt3.ggpht.com/-CDERLAq3BNY7murpWzg3z9Qde3c9ZrRx59LlLEb1UzKDKZ_ckpTAOlYVQ5TJo9XTgJl2kh9bw=s900-c-k-c0x00ffffff-no-rj",
             },
             "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYWFhZDU5OTQtODhmZC00N2Y4LWE0NDgtNDdiZjIyMzEwOTQ5In0.WEEETLqM5Mm-DZTzqo1bfgA743vaFsj-ATX7RcafFuQ" // token from the DataBase
+
         );
         // Creates the public channel, which anyone can join!
         const channel = client.channel("livestream", "public", {name: "Public"});
