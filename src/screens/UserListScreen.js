ee8f2b943656ccc82c34347b3229dbadc8db36a7
Rokas <rookas.rudzenskas@gmail.com>
2022-10-08 15:38:30 +0300

    Rokas

--- src/screens/UserListScreen.js
+++ src/screens/UserListScreen.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import {useChatContext} from "stream-chat-expo";
 
 const UserListScreen = () => {
+    const {client} = useChatContext();
+
+    const fetchUsers = async () => {
+        const response = await client.queryUsers({});
+        console.log(response);
+    }
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
     return (
         <View>
             <Text className="text-white">
